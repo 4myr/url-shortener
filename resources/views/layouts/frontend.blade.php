4e9269f8852415fe204be14602e5e9e822de24dc
AMYR <info@amyr.ir>
2020-06-16 15:27:43 +0430

    Add style, font and navbar

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -14,11 +14,8 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('style/css/main.css?') }}<?php echo time(); ?>">
     @yield('headCSS')
-
-    <style>
-
-    </style>
 
 
     <!-- Fonts -->
@@ -26,6 +23,37 @@
 </head>
 <body>
 <div >
+    <nav class="navbar navbar-expand-sm navbar-dark pb-3" dir="rtl">
+        <a class="navbar-brand pull-right pl-" href="#">
+            <img class="logo img-responsive" src="{{ asset('images/logo.png') }}" alt="URL Shortener">
+        </a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId"
+                aria-controls="collapsibleNavId"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId" dir="rtl">
+            <ul class="navbar-nav ml-auto mt-2 mt-lg-0 text-right">
+                <li class="nav-item active pl-lg-3">
+                    <a class="nav-link" href="#">خانه <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item pl-lg-3">
+                    <a class="nav-link" href="#">درباره ما</a>
+                </li>
+                <li class="nav-item pl-lg-3">
+                    <a class="nav-link" href="#">تماس با ما</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0 text-right">
+                <li class="nav-item pl-lg-3">
+                    <a class="nav-link" href="#">ورود</a>
+                </li>
+                <li class="nav-item pl-lg-3 pr-lg-3">
+                    <a class="nav-link cl-secondary" href="#">ثبت نام</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <div class="content">
         @yield('content')
     </div>
